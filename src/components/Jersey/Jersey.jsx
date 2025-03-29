f7339bf6614a86bb38cc31fc348ce24f9affbb2d
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-03-30 03:25:14 +0600

    added some details in jersey container

--- src/components/Jersey/Jersey.jsx
+++ src/components/Jersey/Jersey.jsx
@@ -4,11 +4,17 @@ import './Jersey.css'
 const Jersey = ({jersey}) => {
     console.log(jersey);
 
-    const {image} = jersey;
+    const {image, team, price, ratings, sizes, quantity} = jersey;
 
     return (
         <div className='jersey-card'>
             <img src={image} alt=''/>
+            <h2>{team}</h2>
+            <p>price: ${price}</p>
+            <p>ratings: {ratings}</p>
+            <h3>Available: {quantity}</h3>
+            {/* <p id='sizes'>sizes: {sizes}</p> */}
+            <button>Buy Now</button>
         </div>
     );
 };
